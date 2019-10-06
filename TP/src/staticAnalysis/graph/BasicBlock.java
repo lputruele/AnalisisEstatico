a06cf21eb51623e9af486774733310d69d881c09
Facundo Molina <facu.molina7@gmail.com>
2019-10-06 16:23:30 -0300

    Computing the leaders and the basic blocks - Ready to start with the CFG

--- TP/src/staticAnalysis/graph/BasicBlock.java
+++ TP/src/staticAnalysis/graph/BasicBlock.java
@@ -1,5 +1,12 @@
 package staticAnalysis.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import miniJava.Conditional;
+import miniJava.Iterative;
+import miniJava.Statement;
+
 /**
  * This class represents a Basic Block, i.e., a sequence of program statements in which 
  * flow of control enters at the beginning and leaves at the end without halt or possibility 
@@ -9,4 +16,22 @@ package staticAnalysis.graph;
  */
 public class BasicBlock {
 
+	private List<Statement> statements; // Program statements that are part of the block
+	
+	/**
+	 * Default constructor
+	 */
+	public BasicBlock() {
+		statements = new ArrayList<Statement>();
+	}
+	
+	/**
+	 * Add an statement to the building block
+	 */
+	public void addStatement(Statement stmt) {
+		if (stmt==null||stmt instanceof Conditional||stmt instanceof Iterative)
+			throw new IllegalArgumentException("Invalid statement");
+		statements.add(stmt);
+	}
+	
 }
--- TP/src/staticAnalysis/graph/ControlFlowGraph.java
+++ TP/src/staticAnalysis/graph/ControlFlowGraph.java
@@ -33,38 +33,79 @@ public class ControlFlowGraph {
 	 */
 	private List<BasicBlock> getBasicBlocks(Program p) {
 		List<Statement> progStmts = p.getStatements();
-		List<Statement> leaders = getLeaders(progStmts);
-		System.out.print("Total leaders: "+leaders.size());
-		// Construct the basic blocks from the leaders
-		List<BasicBlock> basicBlocks = new LinkedList<BasicBlock>();
+		markLeaders(progStmts);
+		// Construct the basic blocks considering the leaders
+		List<BasicBlock> basicBlocks = computeBasicBlocks(null,progStmts);
 		return basicBlocks;
 	}
 	
 	/**
-	 * Get the leaders
+	 * Mark the leaders
 	 */
-	private List<Statement> getLeaders(List<Statement> progStmts) {
-		List<Statement> leaders = new ArrayList<Statement>();
-		// The first statement is a leader
-		leaders.add(progStmts.get(0));
-		boolean nextIsLeader = false;
-		for (int i=1;i<progStmts.size();i++) {
-			Statement s = progStmts.get(i);
-			// Determine if s is a leader
-			if (s instanceof Conditional) {
-				Conditional c = (Conditional)s;
-				leaders.addAll(getLeaders(c.getThenBlock()));
-				leaders.addAll(getLeaders(c.getElseBlock()));
-				nextIsLeader = true;
-			} else if (s instanceof Iterative) {
-				leaders.addAll(((Iterative)s).getDoBlock());
-				nextIsLeader = true;
-			} else if (nextIsLeader){
-				nextIsLeader = false;
-				leaders.add(s);
-			}		
+	private void markLeaders(List<Statement> progStmts) {
+		if (progStmts.size()>0) {
+			// The first statement is a leader
+			progStmts.get(progStmts.size()-1).setIsLeader(true);
+			boolean nextIsLeader = false;
+			for (int i=progStmts.size()-2;i>=0;i--) {
+				Statement s = progStmts.get(i);
+				// Determine if s is a leader
+				if (s instanceof Conditional) {
+					Conditional c = (Conditional)s;
+					markLeaders(c.getThenBlock());
+					markLeaders(c.getElseBlock());
+					nextIsLeader = true;
+				} else if (s instanceof Iterative) {
+					markLeaders(((Iterative)s).getDoBlock());
+					nextIsLeader = true;
+				} else if (nextIsLeader){
+					nextIsLeader = false;
+					s.setIsLeader(true);
+				}	
+			}
 		}
-		return leaders;
+	}
+	
+	/**
+	 * Compute the list of basic blocks from the consider the leader program statements
+	 */
+	private List<BasicBlock> computeBasicBlocks(BasicBlock currBasicBlock,List<Statement> progStmts) {
+		List<BasicBlock> basicBlocks = new ArrayList<BasicBlock>();
+		boolean addCurr = false;
+		int j = progStmts.size()-1;
+		while (j >= 0) {
+			Statement stmt = progStmts.get(j);
+			if (stmt.isLeader()) {
+				if (currBasicBlock!=null) {
+					basicBlocks.add(currBasicBlock);
+				}
+				currBasicBlock = new BasicBlock();
+				currBasicBlock.addStatement(stmt);
+				addCurr = true;
+			} else {
+				if (!stmt.isControlTransferStatement()) {
+					currBasicBlock.addStatement(stmt);
+					addCurr = true;
+				} else {
+					// Is a control transfer statement
+					basicBlocks.add(currBasicBlock);
+					currBasicBlock = null;
+					addCurr = false;
+					if (stmt instanceof Conditional) {
+						Conditional c = (Conditional)stmt;
+						basicBlocks.addAll(computeBasicBlocks(null,c.getThenBlock()));
+						basicBlocks.addAll(computeBasicBlocks(null,c.getElseBlock()));
+					} else if (stmt instanceof Iterative) {
+						Iterative i = (Iterative)stmt;
+						basicBlocks.addAll(computeBasicBlocks(null,i.getDoBlock()));
+					} 
+				}
+			}
+			j--;	
+		}
+		if (addCurr)
+			basicBlocks.add(currBasicBlock);
+		return basicBlocks;
 	}
 	
 	/**
